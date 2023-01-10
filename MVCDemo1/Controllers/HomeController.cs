5c9a717fd1818a9554339b9f943c7ee2948144d8
Akshar1809 <aksharpatel1809@gmail.com>
2023-01-10 18:52:55 +0530

    ViewBag and ViewData added

--- MVCDemo1/Controllers/HomeController.cs
+++ MVCDemo1/Controllers/HomeController.cs
@@ -10,13 +10,28 @@ namespace MVCDemo1.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Countries = new List<string> { 
+                "India",
+                "USA",
+                "Canada",
+                "Australia"
+            };
+
+
+            ViewData["City"] = new List<string> {
+                "Ahmedabad",
+                "CaliforniA",
+                "Toronto",
+                "Sydney"
+            };
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            /*ViewBag.Message = "Your application description page.";*/
 
+            
             return View();
         }
 
